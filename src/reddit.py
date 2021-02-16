c0e927169832db5be9d964fe7d6f4f6f901bdead
= <albvinc.vinc@gmail.com>
2021-02-16 14:53:14 +0000

    All comments including replies are fectched now

--- src/reddit.py
+++ src/reddit.py
@@ -1,92 +1,69 @@
 import praw
 
-redditInstance = praw.Reddit(
-    client_id="jshyL9CMgH5nEg",
-    client_secret="WEHm5M9WLUd7PzjzydlUUKiHSH1vnw",
-    user_agent="sentiment_analysis by u/albinv1")
 
-
-# todo support for more than 100 posts
-def get_submission_from_subreddit(reddit, subreddit_name):
+# todo: support for more than 100 posts
+def get_submission_from_subreddit(reddit, subreddit_name, num_posts=1):
+    """ Returns a list of submission objects found on the specified subreddit """
     # sorters -> relevance, hot, top, new, comments
     # max limit current = 100
-    return reddit.subreddit(subreddit_name).hot(limit=1)
-
-#SIMPLE COMMENT EXTRACTION
-# def get_all_comments(reddit):
-#     all_comments = []
-#     for submission in get_submission_from_subreddit(reddit, "learnpython"):
-#         print(submission.title)
-#         print(submission.selftext)
-#         for comment in submission.comments.list():
-#             all_comments.append(comment.body)
-#     return all_comments
-
-
-# def get_all_comments(comments):
-#     if comments is None:
-#         return []
-#     if hasattr(comments, "body"):
-#         #print(comments.body)
-#         return comments.body
-#     else:
-#         if isinstance(comments, praw.models.reddit.more.MoreComments):
-#             more_comments = comments.comments()
-#             comments_acc = []
-#             for comment in more_comments:
-#                 #print(comment.body)
-#                 comments_acc.append(comment.body)
-#         else:
-#             combined = []
-#             # comments.replace_more(None, 0),
-#             for comment in (comments.list()):
-#                 all_comments = get_all_comments(comment)
-#                 if all_comments:
-#                     combined.append(all_comments)
-#                 if isinstance(comments, praw.models.reddit.comment.Comment):
-#                     all_replies = get_all_comments(comment.replies)
-#                     if all_replies:
-#                         combined.append(all_replies)
-#             return combined
+    return reddit.subreddit(subreddit_name).hot(limit=num_posts)
 
 
 def get_all_comments(comments):
+    """ Given a comment object (i.e CommentForest), outputs a list of all the comments in there """
     if comments is None:
         return []
-    if isinstance(comments, praw.models.reddit.more.MoreComments):
-        return get_all_comments(comments.comments)
-    if isinstance(comments, praw.models.reddit.comment.Comment):
-        #comments.refresh()
+    elif isinstance(comments, praw.models.reddit.more.MoreComments):
+        return get_all_comments(comments.comments())
+    elif isinstance(comments, praw.models.reddit.comment.Comment):
         replies = comments.replies
         return [comments.body, get_all_comments(replies)]
-    if isinstance(comments, praw.models.comment_forest.CommentForest):
+    elif isinstance(comments, praw.models.comment_forest.CommentForest):
         combined = []
-        # comments.replace_more(None, 0),
         for comment in (comments.list()):
-            combined.append(get_all_comments(comment))
+            combined = combined + get_all_comments(comment)
+        return combined
+    elif isinstance(comments, list):
+        combined = []
+        for comment in comments:
+            combined = combined + get_all_comments(comment)
         return combined
     else:
         print(type(comments))
         print(comments)
 
 
+def flatten_list(elem):
+    """ a better flat_list() function than pythons default implementation for this specific use case """
+    if not elem:
+        return []
+    elif isinstance(elem, str):
+        return [elem]
+    elif isinstance(elem, list):
+        combined = []
+        for element in elem:
+            flat = flatten_list(element)
+            if flat:
+                combined = combined + flat
+        return combined
+    else:
+        print(type(elem))
+        print(elem)
 
 
-
-for submission in get_submission_from_subreddit(redditInstance, "learnpython"):
-    print(submission)
-    print(submission.title)
-
-    x = get_all_comments(submission.comments)
-
-    for comment in x:
-        print(comment)
+def get_all_comments_from_subreddit(reddit, subreddit_name):
+    """ Given a reddit instance and a subreddit name, will fecth all comments from there"""
+    comments_acc = set()
+    for submission in get_submission_from_subreddit(reddit, subreddit_name):
+        comments = get_all_comments(submission.comments)
+        comments_acc.update(flatten_list(comments))
+    return list(comments_acc)
 
 
+redditInstance = praw.Reddit(
+    client_id="jshyL9CMgH5nEg",
+    client_secret="WEHm5M9WLUd7PzjzydlUUKiHSH1vnw",
+    user_agent="sentiment_analysis by u/albinv1")
 
-
-    # all = get_all_comments(submission.comments)
-    # print(all)
-
-
-    print("\n")
+for elem in get_all_comments_from_subreddit(redditInstance, "learnpython"):
+    print(elem)
